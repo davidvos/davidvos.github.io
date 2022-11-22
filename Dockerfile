0d5bd367f13904582b452876c1f623ef1780e8ec
David Vos <vos.dja@gmail.com>
2022-11-22 14:56:09 +0100

    github actions

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM jekyll/jekyll:latest
-
-# Setup you server
-
-# This container exposes port 8080 to the outside world
-EXPOSE 4000
--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:4000',
+    baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
