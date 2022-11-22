4a319e1d8184265aa1eda4bc580f8b51d8c3fe44
David Vos <vos.dja@gmail.com>
2022-11-22 14:21:07 +0100

    add three tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
+  })
+})
+
+describe('Click Chrome Plugin Link', () => {
+  it('Chrome plugin link found and clicked', () => {
+    cy.get('.plugin-link').click()
+  })
+})
+
+describe('Click A-Insights Link', () => {
+  it('A-Insights found and clicked', () => {
+    cy.get('.ainsights-link').click()
   })
 })
