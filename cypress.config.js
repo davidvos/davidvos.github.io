44d997901a35ecbdfbd4fbc48f731700eb17ad7b
David Vos <vos.dja@gmail.com>
2023-01-23 16:17:00 +0100

    cv

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "e66qit",
   e2e: {
     baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
